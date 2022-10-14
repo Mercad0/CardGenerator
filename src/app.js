e0d6baa8adcfe4d68b1bdacafb8fd0d6e2d19826
Mercad0 <abimaelmercado05@gmail.com>
2022-10-14 15:39:34 +0000

    card generator, needs minor tweaks

--- src/app.js
+++ src/app.js
@@ -2,10 +2,45 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//write your code here
 
+// function magic() {
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#topSuit").innerHTML = magic();
+  document.querySelector("#bottomSuit").innerHTML = magic();
+  document.querySelector("#luckyNumbers").innerHTML = randomNumbers();
 };
+//   document.getElementById("#passButton").addEventListener("click", magic);
+// }
+
+let randomNumbers = function() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "LOL"
+  ];
+  let theseNumb = Math.floor(Math.random() * numbers.length);
+  return numbers[theseNumb];
+};
+
+let randomSuit = function() {
+  // let suit = ["♥", "♦", "♠", "♣"]; these are in order displayed below
+  let suit = ["&#9829;", "&#9830;", "&#9824;", "&#9827"];
+  let theseSuit = Math.floor(Math.random() * suit.length);
+  return suit[theseSuit];
+};
+
+let magic = randomSuit;
+
+// document.getElementById("passButton").addEventListener("click", clicked);
