e5331b0a1056fd29c14c5a88129b286eaac579b0
Mercad0 <111986880+Mercad0@users.noreply.github.com>
2022-10-14 20:13:16 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ let randomSuit = function() {
   // let suit = ["♥", "♦", "♠", "♣"];
   // these are in order displayed below
   let suit = ["&#10084;&#65039;", "&#128310;", "&#9824;", "&#9827"];
+  // check
   let theseSuit = Math.floor(Math.random() * suit.length);
   return suit[theseSuit];
 };
