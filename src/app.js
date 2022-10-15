d2e905f953244b868e1e63a3d11b7e500fb4d8e1
Mercad0 <abimaelmercado05@gmail.com>
2022-10-15 00:01:12 +0000

    added red heart & orange diamond emoji "nobodys gonna know!!!"

--- src/app.js
+++ src/app.js
@@ -34,8 +34,9 @@ let randomNumbers = function() {
 };
 
 let randomSuit = function() {
-  // let suit = ["♥", "♦", "♠", "♣"]; these are in order displayed below
-  let suit = ["&#9829;", "&#9830;", "&#9824;", "&#9827"];
+  // let suit = ["♥", "♦", "♠", "♣"];
+  // these are in order displayed below
+  let suit = ["&#10084;&#65039;", "&#128310;", "&#9824;", "&#9827"];
   let theseSuit = Math.floor(Math.random() * suit.length);
   return suit[theseSuit];
 };
